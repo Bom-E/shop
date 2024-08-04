1b129eaacfd79a289d7a225103e320634c94c000
Bom-E <j32575086@gmail.com>
2024-08-04 23:07:01 +0900

    240804 ui start

---
+++ src/main/front/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import UserHeader from './components/user/UserHeader';
+
+function App() {
+  return (
+    <div className="App container-fluid">
+      <UserHeader/>      
+    </div>
+  );
+}
+
+export default App;
