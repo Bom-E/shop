d0657e9b7b61f7f772b2355c73e96f400c5bec33
Bom-E <j32575086@gmail.com>
2024-08-07 21:35:04 +0900

    gridslide f

--- src/main/front/src/pages/Home.js
+++ src/main/front/src/pages/Home.js
@@ -1,9 +1,12 @@
+import GridSlider from "../components/common/GridSlider";
+
 const Home = () => {
     return(
-        <>
-        
-        </>
+    <div>
+        <GridSlider/>
+    </div>
     )
 };
 
 export default Home;
+
